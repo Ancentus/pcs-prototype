68f1741824b031a192e3df1b91169f5404a4f497
Ancentus <mancentus7@gmail.com>
2023-04-01 01:11:46 +0300

    Write UserrepositoryTest

---
+++ src/test/java/com/dawapal/pcs/repository/UserRepositoryTests.java
@@ -0,0 +1,41 @@
+package com.dawapal.pcs.repository;
+
+import com.dawapal.pcs.model.User;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+
+public class UserRepositoryTests {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repo;
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setLogin_name("ancentus");
+        user.setPassword("1234");
+        user.setFull_name("Ancentus Makau");
+        user.setUser_type("ems");
+
+        User savedUser = repo.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getUser_id());
+
+        assertThat(user.getLogin_name()).isEqualTo(existUser.getLogin_name());
+
+    }
+}
