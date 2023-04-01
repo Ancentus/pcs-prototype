c1b1f86cac3e8c0d1b5bf6e7a904e808931c4cc1
Ancentus <mancentus7@gmail.com>
2023-04-01 20:16:07 +0300

    Create REST controller

---
+++ src/main/java/com/dawapal/pcs/controller/PCSController.java
@@ -0,0 +1,15 @@
+package com.dawapal.pcs.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ancentus
+ * @version 1.0.0
+ */
+
+@CrossOrigin(maxAge = 3600)
+@RestController
+public class PCSController {
+    
+}
