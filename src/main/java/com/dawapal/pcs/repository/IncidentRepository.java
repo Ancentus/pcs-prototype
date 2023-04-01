3d5a271caac147a3bcc403452f6aab814cf55433
Ancentus <mancentus7@gmail.com>
2023-04-01 22:03:27 +0300

    Create Incident repository

---
+++ src/main/java/com/dawapal/pcs/repository/IncidentRepository.java
@@ -0,0 +1,16 @@
+package com.dawapal.pcs.repository;
+
+import com.dawapal.pcs.model.Incident;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Ancentus Makau
+ * @version 1.0.0
+ */
+
+@Repository
+public interface IncidentRepository extends JpaRepository<Incident, Long>{
+    
+}
